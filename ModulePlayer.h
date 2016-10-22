e18287ec8209445ec624bdf53c704f18e8a31fde
carcasanchez <carcasanchez@gmail.com>
2016-10-22 12:35:38 +0200

    Reverted to previous state

--- ModulePlayer.h
+++ ModulePlayer.h
@@ -1,10 +1,8 @@
 #pragma once
 #include "Module.h"
-#include "p2List.h"
-#include "p2Point.h"
 #include "Globals.h"
-class PhysBody;
-struct SDL_Texture;
+#include "p2Point.h"
+
 class ModulePlayer : public Module
 {
 public:
@@ -15,31 +13,8 @@ public:
 	update_status Update();
 	bool CleanUp();
 
-	void OnCollision(PhysBody* bodyA, PhysBody* bodyB);
-
 public:
-	p2List<PhysBody*> circles;
-	p2List<PhysBody*> boxes;
-
-	PhysBody* sensor;
-	bool sensed;
-
-	
-
-	SDL_Texture* ball;
-	SDL_Texture* left_flip;
-	SDL_Texture* right_flip;
-	
-	p2List_item<PhysBody*>* flips_start;
-
 
 	unsigned int score;
-
-
-	PhysBody* mid_left_flip;
-	PhysBody* mid_right_flip;
-	uint bonus_fx;
-	p2Point<int> ray;
-	bool ray_on;
 
 };
--- ModuleSceneIntro.h
+++ ModuleSceneIntro.h
@@ -43,11 +43,12 @@ public:
 
 	
 	SDL_Texture* background;
-
 	SDL_Texture* ball;
 	SDL_Texture* left_flip;
 	SDL_Texture* right_flip;
 
+
+	p2List_item<PhysBody*>* flips_start;
 
 	PhysBody* mid_left_flip;
 	PhysBody* mid_right_flip;
